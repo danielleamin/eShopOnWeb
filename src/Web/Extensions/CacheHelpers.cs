902fcbe4cc7fcf4044c9e5cb05d6ac55139b148d
Danielle Amin <contato.daniellearaujo@gmail.com>
2020-01-31 11:15:28 +0000

    filt

--- src/Web/Extensions/CacheHelpers.cs
+++ src/Web/Extensions/CacheHelpers.cs
@@ -1,22 +1,33 @@
-﻿using Microsoft.eShopWeb.Web;
-using Microsoft.eShopWeb.Web.Extensions;
-using Xunit;
+﻿using System;
 
-namespace Microsoft.eShopWeb.UnitTests.Web.Extensions.CacheHelpersTests
+namespace Microsoft.eShopWeb.Web.Extensions
 {
-    public class GenerateCatalogItemCacheKey_Should
+    public static class CacheHelpers
     {
-        [Fact]
-        public void ReturnCatalogItemCacheKey()
+        public static readonly TimeSpan DefaultCacheDuration = TimeSpan.FromSeconds(30);
+        private static readonly string _itemsKeyTemplate = "items-{0}-{1}-{2}-{3}-{4}";
+
+        public static string GenerateCatalogItemCacheKey(int pageIndex, int itemsPage,
+            string searchText, int? brandId, int? typeId)
         {
-            var pageIndex = 0;
-            string searchText = null;
-            int? brandId = null;
-            int? typeId = null;
+            return string.Format(
+                _itemsKeyTemplate, pageIndex, itemsPage, brandId, typeId,
+                searchText ?? string.Empty // TODO: Handle invalid special chars in cache keys?
+            );
+        }
 
-            var result = CacheHelpers.GenerateCatalogItemCacheKey(pageIndex, Constants.ITEMS_PER_PAGE, searchText, brandId, typeId);
+        public static string GenerateCatalogItemIdKey(int id) {
+            return $"catalog_item_{id}";
+        }
 
-            Assert.Equal("items-0-10--", result);
+        public static string GenerateBrandsCacheKey()
+        {
+            return "brands";
+        }
+
+        public static string GenerateTypesCacheKey()
+        {
+            return "types";
         }
     }
 }
