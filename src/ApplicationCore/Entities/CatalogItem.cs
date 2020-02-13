d526fb19aca6b37a27d53f994bfed5a4b0b84cc6
Danielle Amin <contato.daniellearaujo@gmail.com>
2020-02-13 09:44:49 +0000

    changes

--- src/ApplicationCore/Entities/CatalogItem.cs
+++ src/ApplicationCore/Entities/CatalogItem.cs
@@ -10,7 +10,12 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities
         public decimal Price { get; set; }
         public string PictureUri { get; set; }
         public int CatalogTypeId { get; set; }
+
         public int CatalogBrandId { get; set; }
+
+        public int CatalogStoreId  { get; set; }
+
+        
 
         [DefaultValue(true)]
         public bool ShowPrice { get; set; } = true;
@@ -19,6 +24,10 @@ namespace Microsoft.eShopWeb.ApplicationCore.Entities
         #region "Navigation properties"
         public CatalogType CatalogType { get; set; }
         public CatalogBrand CatalogBrand { get; set; }
+
+        public CatalogStore CatalogStore { get; set; }
+
+        public CatalogStock CatalogStock { get; set; }
         #endregion
 
     }
